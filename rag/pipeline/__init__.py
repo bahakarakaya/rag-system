96e67fed9c324153201dead3c7383599c88f83c7
bahakarakaya <bhkrky@gmail.com>
2026-03-08 17:23:27 +0300

    fix: remove EvaluationPipeline import from pipeline module

--- rag/pipeline/__init__.py
+++ rag/pipeline/__init__.py
@@ -1,5 +1,4 @@
 from .ingestion import IngestionPipeline
 from .query import QueryPipeline
 from .generation import GenerationPipeline
-from .evaluation import EvaluationPipeline
 from .reranker import CrossEncoderReranker
